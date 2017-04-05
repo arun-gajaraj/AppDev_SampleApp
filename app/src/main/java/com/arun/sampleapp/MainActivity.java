3002159313065c60d9b1b5464b3a77b30652d188
Arun G <arunamigo@live.com>
2017-04-05 23:29:20 +0530

    EditText, Toast, Button

--- app/src/main/java/com/arun/sampleapp/MainActivity.java
+++ app/src/main/java/com/arun/sampleapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.arun.sampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.widget.Toast.makeText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view)
+    {
+        EditText nameText = (EditText) findViewById(R.id.nameField);
+        String name = nameText.getText().toString();
+        makeText(MainActivity.this, "Hi " + name + " !", Toast.LENGTH_SHORT).show();
     }
 }
